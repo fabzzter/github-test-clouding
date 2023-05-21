ad599f66bc2ee61cbbac4c424f3aac7ba3d5b77f
fabzzter <blader07@gmail.com>
2023-05-20 19:12:17 -0500

    [devops][feature]: Tipos de objetos disponibles en Python  Changes to be committed: 	new file:   clase-8/objects.py

---
+++ clase-8/objects.py
@@ -0,0 +1,10 @@
+list = ["valor 1", 2, 23.50, True, {"Nombre":"Fabrizzio"}]
+tuple = ("valor 1", 2, 23.50, True, {"Nombre":"Fabrizzio"}) #No se pueden modificar
+dictionary = { #llave:valor
+    "key1":"string",
+    "key2":1,
+    "key3":[1,2,3,4,5],
+    "key4":True}
+print(list)
+print(tuple)
+print(dictionary)
